489ab0375460f0f24be8bb298317fd95c52bcbe9
Symion Edwards <symion.edwards@gmail.com>
2020-10-29 16:57:22 +0000

    Written RSpec tests for docking bikes

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -3,4 +3,11 @@ class DockingStation
   def release_bike
     Bike.new
   end
+
+  def dock(bike)
+    bike
+  end
+
+  def bike
+  end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -9,3 +9,22 @@ describe DockingStation do
     expect(subject.release_bike).to be_working
   end
 end
+
+describe DockingStation do
+  it "It docks something" do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq bike
+  end
+end
+
+describe DockingStation do 
+  it {is_expected.to respond_to :bike}
+end
+
+describe DockingStation do
+  it "returns docked bikes" do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
+  end
+end
