750e1935af9bbc95d1b38aa4e005f559526e284f
Margarida Martelo <margmartelo@gmail.com>
2021-05-14 17:36:41 +0100

    Add test for releasing an instance of a Bike class when release_bike method is called

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,6 +1,12 @@
 require 'docking_station'
+require 'bike'
 
 describe DockingStation do 
     it { is_expected.to respond_to :release_bike }   
+
+    it "should release an instance of a Bike class when release_bike method is called" do
+        expect(subject.release_bike).to be_a(Bike)
+    end
+
 end
 
