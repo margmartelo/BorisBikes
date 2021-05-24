7d3f122b0d622118fde4ce65d0e24ff3510e6bde
Margarida Martelo <margmartelo@gmail.com>
2021-05-24 19:02:03 +0100

    Refactoring dock method in order to apply Single Responsibility Principle

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -9,12 +9,21 @@ class DockingStation
     end
 
     def release_bike
-        fail "Sorry, there are no bikes available at the moment." unless @bikes.length != 0
+        fail "Sorry, there are no bikes available at the moment." if empty?
         @bikes.pop 
     end
 
     def dock(bike)
-        fail "It's not possible to dock; station at maximum capacity." if @bikes.length >= 20
+        fail "It's not possible to dock; station at maximum capacity." if full? 
         @bikes.push(bike)
+    end
+
+    private 
+    def full?
+        @bikes.length >= 20
+    end
+
+    def empty?
+        @bikes.length == 0
     end
 end
