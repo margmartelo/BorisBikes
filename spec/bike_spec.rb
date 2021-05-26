55f0dd0d0c12196aca31e7cd3e7fb388da582ad4
Margarida Martelo <margmartelo@gmail.com>
2021-05-26 23:47:15 +0100

    Reporting a bike as broken

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,7 +1,14 @@
 require 'bike'
 
 describe Bike do
-  it { is_expected.to respond_to :working? }
+
+  it "should identify a working bike" do
+    bike = Bike.new
+    expect(bike.working?).to be true
+  end 
+
+  it "allows a broken bike to be reported" do
+      subject.report_broken
+      expect(subject).to be_broken
+    end
 end
-
-
