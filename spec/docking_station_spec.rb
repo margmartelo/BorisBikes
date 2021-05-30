81085d46cbb88a9a308ecb6e4ad126eb041acf02
Margarida Martelo <margmartelo@gmail.com>
2021-05-30 15:42:21 +0100

    Isolating tests with doubles and mocking behaviour on doubles

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,51 +1,45 @@
 require 'docking_station'
 
 describe DockingStation do 
+    let(:bike) { double :bike }
     it { is_expected.to respond_to :release_bike }   
 
     describe '#release_bike' do
-       it "should release a bike" do
-          subject.dock(Bike.new)
-          subject.release_bike
-          expect(subject.bikes.length).to eq 0
-       end
+        it "should release bikes that are working" do
+            allow(bike).to receive(:broken?).and_return(false)
+            subject.dock(bike)
+            released_bike = subject.release_bike
+            expect(released_bike.broken?).to be false
+        end 
 
        it "shouldn't release a broken bike" do
-            bike = Bike.new
-            bike.report_broken
+            allow(bike).to receive(:broken?).and_return(true)
             subject.dock(bike)
             expect { subject.release_bike }.to raise_error("Sorry, there are no bikes available at the moment.")
        end
 
        it "should raise an error if no bikes are docked at the station" do
-          station = DockingStation.new
-          expect { station.release_bike }.to raise_error("Sorry, there are no bikes available at the moment.")
+            station = DockingStation.new
+            expect { station.release_bike }.to raise_error("Sorry, there are no bikes available at the moment.")
        end
     
     end
 
-    it "should release bikes that are working" do
-        bike = Bike.new
-        expect(bike.broken?).to be false
-    end 
-
     it "should allow to dock a working bike to a docking station" do
-        bike = Bike.new
         subject.dock(bike)
         expect(subject.bikes).to include(bike) 
     end
     
     it "should allow to dock a broken bike to a docking station" do
-        bike = Bike.new
-        bike.report_broken
+        allow(bike).to receive(:broken?).and_return(false)
         subject.dock(bike)
         expect(subject.bikes).to include(bike) 
     end
 
     it "should allow to see docked bikes" do
-        subject.dock(Bike.new)
+        subject.dock(bike)
         expect(subject.bikes.length).to eq 1
-        10.times { subject.dock Bike.new }
+        10.times { subject.dock double(:bike) }
         expect(subject.bikes.length).to eq 11   
     end
 
@@ -61,8 +55,8 @@ describe DockingStation do
 
 
     it "should raise an error in the case dock is at its maximum capacity" do 
-        subject.capacity.times { subject.dock Bike.new }
-        expect { subject.dock(Bike.new) }.to raise_error("It's not possible to dock; station at maximum capacity.")
+        subject.capacity.times { subject.dock double(:bike) }
+        expect { subject.dock(double(:bike)) }.to raise_error("It's not possible to dock; station at maximum capacity.")
     end
 
 end
